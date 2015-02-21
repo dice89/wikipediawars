886774200205f3834ebfd8a170bf98a25775324e
fuchsvomwalde <jascha.quintern@googlemail.com>
2015-02-21 14:38:47 +0100

    Little Update (New UI)

---
+++ public/scripts/language-pack.js
@@ -0,0 +1,7 @@
+var l = 'EN'; // selected language
+var _ = {
+    job_interpretation: {
+        DE: "Designer · Programmierer · Creative Artist",
+        EN: "Designer · Programmer · Creative Artist"
+    }
+};
